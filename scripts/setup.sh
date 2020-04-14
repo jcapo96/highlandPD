3530579c4161d7ee45a689d5849d60102c143733
Anselmo Cervera <acervera@fnal.gov>
2020-04-14 15:48:23 +0200

    set the path for all packages and the package hierarchy

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,5 @@
 # and this package to the highland package hierarchy (otherwise the parameters file will not be read)
-#export HIGHLAND_PACKAGE_HIERARCHY=pionAnalysis:$HIGHLAND_PACKAGE_HIERARCHY
+export HIGHLAND_PACKAGE_HIERARCHY=pionAnalysis:pdBaseAnalysis:LArSoftReader:pdUtils:pdEventModel:$HIGHLAND_PACKAGE_HIERARCHY
 
 # automatically find the highland source directory
 export HIGHLANDPDPATH="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")/.." && pwd -P)"
@@ -8,3 +8,20 @@ echo "HIGHLANDPDPATH=${HIGHLANDPDPATH}"
 export PATH=$HIGHLANDPDPATH/bin:$PATH    
 export DYLD_LIBRARY_PATH=$HIGHLANDPDPATH/lib:$DYLD_LIBRARY_PATH
 export   LD_LIBRARY_PATH=$HIGHLANDPDPATH/lib:$LD_LIBRARY_PATH
+
+
+
+#------ set the ROOT folder for every package in highland  ------
+D0=$PWD
+cd $HIGHLANDPDPATH/src
+for D2 in *
+do
+    if [ ! -d $D2 ]; then
+        continue
+    fi
+    D3=$(echo $D2 | tr "[a-z]" "[A-Z]")
+    D4=${D3}ROOT
+    export ${D4}=$HIGHLANDPDPATH/src/$D2
+done
+cd $D0
+
