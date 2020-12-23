aa55ef970f9966ecea96863e46984012d03e4790
Anselmo Cervera <acervera@dunegpvm12.fnal.gov>
2020-12-23 11:46:20 -0600

    add config files for eigen, protobuf and tensorflow

---
+++ cmake/modules/protobuf-config.cmake
@@ -0,0 +1,4 @@
+# Locates the tensorFlow library and include directories.
+
+set(PROTOBUF_LIBRARIES    /cvmfs/larsoft.opensciencegrid.org/products/protobuf/v3_11_2a/Linux64bit+2.6-2.12-e19/lib64/libprotobuf.so.3.11.2.0)
+set(PROTOBUF_INCLUDE_DIRS /cvmfs/larsoft.opensciencegrid.org/products/protobuf/v3_11_2a/Linux64bit+2.6-2.12-e19/include/)
