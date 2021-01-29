fcd61a18dadcbb8ee0a6936f570940a4c68470da
Anselmo Cervera <acervera@fnal.gov>
2021-01-29 17:07:39 +0100

    use the true-reco association done in EventWeightBase

--- src/pdSystematics/src/TrackEffWeight.cxx
+++ src/pdSystematics/src/TrackEffWeight.cxx
@@ -15,39 +15,27 @@ TrackEffWeight::TrackEffWeight():EventWeightBase(), BinnedParams(std::string(get
 //********************************************************************
 Weight_h TrackEffWeight::ComputeWeight(const ToyExperiment& toy, const AnaEventC& event, const ToyBoxB& box){
 //********************************************************************
-
-  (void)box;
-
-
-  // Get the SystBox for this event, and the appropriate selection and branch
-  SystBoxB* SystBox = GetSystBox(event,box.SelectionEnabledIndex,box.SuccessfulBranch);
-
-
-  BinnedParamsParams params;
-
+  
   // Initialy the weight is 1
   Weight_h eventWeight = 1;
 
+  // Get the tracking efficiency for bin 0 (there is a single bin);
+  BinnedParamsParams params;
+  int index;
+  if(!GetBinValues(0.5, params, index))	return eventWeight;
+
+  // Get the SystBox for this event, and the appropriate selection and branch
+  SystBoxB* SystBox = GetSystBox(event,box.SelectionEnabledIndex,box.SuccessfulBranch);
+   
   // Loop over all TrueParticles in the TPC
   for (Int_t itrue=0;itrue< SystBox->nRelevantTrueObjects; itrue++){      
     AnaTrueParticlePD* truePart = static_cast<AnaTrueParticlePD*>(SystBox->RelevantTrueObjects[itrue]);            
 
-    // Get the tracking efficiency for bin 0 (there is a single bin);
-    int index;
-    if(!GetBinValues(0.5, params, index))	  continue;
+    // True-reco association done only once per event in EventWeightBase;
+    bool found = (SystBox->RelevantTrueObjectsReco[itrue]!=NULL);
 
-    bool found=false;
-    for (Int_t irec=0;irec<(int) SystBox->nRelevantRecObjects; irec++){
-      AnaParticlePD* part = static_cast<AnaParticlePD*>(SystBox->RelevantRecObjects[irec]);
-      if (!part->TrueObject) continue;
-
-      if(truePart->ID==part->TrueObject->ID){            
-        found=true;        
-        break;
-      }
-    }
+    // compute the weight
     eventWeight *= systUtils::ComputeEffLikeWeight(found, toy, GetIndex(), index, params);
-
   }
 
   return eventWeight;
