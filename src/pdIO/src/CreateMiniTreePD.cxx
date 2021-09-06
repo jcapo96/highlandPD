d6a6a4cb721624afc4ee2b3c4b8361170c935235
Anselmo Cervera <acervera@fnal.gov>
2021-09-06 16:06:56 +0200

    properly add the MiniTree converter

--- src/pdIO/src/CreateMiniTreePD.cxx
+++ src/pdIO/src/CreateMiniTreePD.cxx
@@ -6,6 +6,6 @@
 CreateMiniTreePD::CreateMiniTreePD(int argc, char *argv[]):CreateMiniTree(argc, argv){
   //********************************************************************
 
-  input().AddConverter("minitree",         new pdMiniTreeConverter("highlandana/MiniTree"));
+  input().AddConverter("minitree",         new pdMiniTreeConverter());
   input().AddConverter("pduneana",         new PDSPAnalyzerTreeConverter());
 }
