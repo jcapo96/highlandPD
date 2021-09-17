2f62880a61cf5dd4b75cafff2e3120be5f218823
Miguel Garcia Peris <miagarc@dunegpvm10.fnal.gov>
2021-09-17 10:09:09 -0500

    delete kaoncosmicanalysis

--- src/kaonCosmicAnalysis/app/RunKaonCosmicAnalysis.cxx
+++
@@ -1,8 +0,0 @@
-#include "kaonCosmicAnalysis.hxx"
-#include "AnalysisLoop.hxx"
-
-int main(int argc, char *argv[]){
-  kaonCosmicAnalysis* ana = new kaonCosmicAnalysis();
-  AnalysisLoop loop(ana, argc, argv); 
-  loop.Execute();
-}
