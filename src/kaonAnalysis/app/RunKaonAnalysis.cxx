2c8bf3e653bfed41d06ee6ebd30f8ff1bbd023d3
Miguel Garcia Peris <miagarc@dunegpvm12.fnal.gov>
2021-09-22 11:24:35 -0500

    comment

--- src/kaonAnalysis/app/RunKaonAnalysis.cxx
+++
@@ -1,8 +0,0 @@
-#include "kaonAnalysis.hxx"
-#include "AnalysisLoop.hxx"
-
-int main(int argc, char *argv[]){
-  kaonAnalysis* ana = new kaonAnalysis();
-  AnalysisLoop loop(ana, argc, argv); 
-  loop.Execute();
-}
--- src/kaonAnalysis/app/RunKaonCreateMiniTree.cxx
+++
@@ -1,6 +0,0 @@
-#include "kaonCreateMiniTree.hxx"
-
-int main(int argc, char *argv[]){
-  kaonCreateMiniTree ana(argc, argv);
-  ana.Execute();
-}
