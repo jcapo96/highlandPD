04825093c7aaf5f0782708290474ac0d3fcb36c3
Anselmo Cervera <acervera@fnal.gov>
2021-09-24 12:24:02 +0200

    inherit from CreateMiniTreePD and simplify

--- src/secondaryKaonAnalysis/src/kaonCreateMiniTree.cxx
+++ src/secondaryKaonAnalysis/src/kaonCreateMiniTree.cxx
@@ -6,89 +6,9 @@
 #include <iomanip>
 
 //********************************************************************
-kaonCreateMiniTree::kaonCreateMiniTree(int argc, char *argv[]):SimpleLoopBase(argc, argv){
+kaonCreateMiniTree::kaonCreateMiniTree(int argc, char *argv[]):CreateMiniTree(argc, argv){
 //********************************************************************
 
-  // Add the package version (not yet properly done)
-  ND::versioning().AddPackage("secondaryKaonAnalysis", anaUtils::GetSoftwareVersionFromPath((std::string)getenv("SECONDARYKAONANALYSISROOT")));
-  
-  // add the different converters
-  input().AddConverter("minitree", new HighlandMiniTreeConverter("highlandana/MiniTree",true));
-
-}
-
-//********************************************************************
-bool kaonCreateMiniTree::Initialize(){
-//********************************************************************
-  
-  _totalRecoObjects = 0;
-  _savedRecoObjects = 0;
-  _totalTrueObjects = 0;
-  _savedTrueObjects = 0;
-  
-  _saveCandidates=true;
-
-  return true;
-}
-
-//********************************************************************
-void kaonCreateMiniTree::DefineOutputTree(){
-//********************************************************************
-
-  // Add a new tree called "MiniTree"
-  AddTreeWithName(minitree,"MiniTree");
-
-  SetCurrentTree(minitree);
-  SetFillSingleTree(GetCurrentTree());
-
-  // The full AnaSpill. _spill cannot be undefined here, otherwise it give problems in some platforms 
-  _spill=NULL; 
-  GetTree(minitree)->Branch("Spill", "AnaSpill", &_spill,64000,1);
-}
-
-
-//********************************************************************
-bool kaonCreateMiniTree::Process(){
-//********************************************************************
-
-  // Set the tree to fill
-  SetFillSingleTree(minitree);
-  SetCurrentTree(minitree);
-  
-  // Get the corrected spill and set the branch address
-  _spill = static_cast<AnaSpill*>(&input().GetCorrectedSpill());
-  GetTree(minitree)->SetBranchAddress("Spill",&_spill);  
-
-  // The spill is saved always but the ammount of information is reduced
-  DeleteUninterestingParticles();
-  if(_spill->GetIsMC())DeleteUninterestingTrueParticles();
-  // Fill the minitree
-  FillMiniTree();
-    
-  return true;
-}
-
-//********************************************************************
-void kaonCreateMiniTree::Finalize(){
-//********************************************************************
-
-  // Delete Spills for the last entry
-  input().DeleteSpill(); 
-
-  std::cout << std::setprecision(2);
-  std::cout << "Reco filtering factor " << 100*double(_savedRecoObjects)/_totalRecoObjects << "%" << std::endl; 
-  std::cout << "Truth filtering factor " << 100*double(_savedTrueObjects)/_totalTrueObjects << "%" << std::endl; 
-}
-
-//********************************************************************
-void kaonCreateMiniTree::FillMiniTree(){
-//********************************************************************
-
-  // Copy arrays into std::vectors
-  _spill->CopyArraysIntoVectors();
-
-  // Fill the tree
-  FillTree(minitree);
 }
 
 //********************************************************************
@@ -135,7 +55,7 @@ void kaonCreateMiniTree::DeleteUninterestingParticles(){
 //********************************************************************
 
   AnaBunchB* bunch = static_cast<AnaBunchB*>(_spill->Bunches[0]);
-  _totalRecoObjects += bunch->Particles.size();
+  _totalParticles += bunch->Particles.size();
 
   std::vector<AnaParticleB*> goodParticles;
   std::vector<AnaParticleB*> badParticles; 
@@ -149,15 +69,17 @@ void kaonCreateMiniTree::DeleteUninterestingParticles(){
       AnaParticleB* dau = static_cast<AnaParticleB*>(part->Daughters[0]);
       AnaParticleB* parent = (anaUtils::GetParticleByID(*bunch, part->ParentID));
       if(std::find(goodParticles.begin(), goodParticles.end(), part) == goodParticles.end())
-	goodParticles.push_back(static_cast<AnaParticleB*>(part));
-      if(dau)if(std::find(goodParticles.begin(), goodParticles.end(), dau) == goodParticles.end())
-	       goodParticles.push_back(dau);
-      if(parent)if(std::find(goodParticles.begin(), goodParticles.end(), parent) == goodParticles.end())
-		  goodParticles.push_back(parent); 
+        goodParticles.push_back(static_cast<AnaParticleB*>(part));
+      if(dau)
+        if(std::find(goodParticles.begin(), goodParticles.end(), dau) == goodParticles.end())
+          goodParticles.push_back(dau);
+      if(parent)
+        if(std::find(goodParticles.begin(), goodParticles.end(), parent) == goodParticles.end())
+          goodParticles.push_back(parent); 
     }
     else{
       if(std::find(goodParticles.begin(), goodParticles.end(), part) == goodParticles.end())
-	badParticles.push_back(part);
+        badParticles.push_back(part);
     }
   }
   for(std::vector<AnaParticleB*>::iterator it = badParticles.begin(); it != badParticles.end(); it++){      
@@ -165,7 +87,7 @@ void kaonCreateMiniTree::DeleteUninterestingParticles(){
   }
 
   bunch->Particles = goodParticles;
-  _savedRecoObjects += bunch->Particles.size();
+  _savedParticles += bunch->Particles.size();
 }
 
 //********************************************************************
@@ -178,7 +100,7 @@ void kaonCreateMiniTree::DeleteUninterestingTrueParticles(){
 
   std::set<AnaTrueParticleB*> goodTrueParticles;
   std::vector<AnaTrueParticleB*> badTrueParticles;
-  _totalTrueObjects += _spill->TrueParticles.size();
+  _totalTrueParticles += _spill->TrueParticles.size();
 
 
   AnaTrueParticle* beampart = pdAnaUtils::FindBeamTrueParticle(*_spill);
@@ -190,7 +112,7 @@ void kaonCreateMiniTree::DeleteUninterestingTrueParticles(){
     AnaParticleB* part = *it;  
     if(part->TrueObject)
       if(std::find(goodTrueParticles.begin(), goodTrueParticles.end(), part->TrueObject) == goodTrueParticles.end())
-	goodTrueParticles.insert(static_cast<AnaTrueParticleB*>(part->TrueObject));
+        goodTrueParticles.insert(static_cast<AnaTrueParticleB*>(part->TrueObject));
   }
   
   // Loop over all true particles in the spill
@@ -198,19 +120,21 @@ void kaonCreateMiniTree::DeleteUninterestingTrueParticles(){
     AnaTrueParticlePD* truepart = static_cast<AnaTrueParticlePD*>(*it);
     if(truepart->PDG==321){
       if(std::find(goodTrueParticles.begin(), goodTrueParticles.end(), truepart) == goodTrueParticles.end())
-	goodTrueParticles.insert(static_cast<AnaTrueParticleB*>(truepart));
+        goodTrueParticles.insert(static_cast<AnaTrueParticleB*>(truepart));
       for(int i = 0; i < (int)truepart->Daughters.size(); i++){
-	AnaTrueParticlePD* truedau = pdAnaUtils::GetTrueParticle(_spill->TrueParticles,truepart->Daughters[i]);
-	if(truedau)if(std::find(goodTrueParticles.begin(), goodTrueParticles.end(), truedau) == goodTrueParticles.end())
-		     goodTrueParticles.insert(static_cast<AnaTrueParticleB*>(truedau));
+        AnaTrueParticlePD* truedau = pdAnaUtils::GetTrueParticle(_spill->TrueParticles,truepart->Daughters[i]);
+        if(truedau)
+          if(std::find(goodTrueParticles.begin(), goodTrueParticles.end(), truedau) == goodTrueParticles.end())
+            goodTrueParticles.insert(static_cast<AnaTrueParticleB*>(truedau));
       }
       AnaTrueParticlePD* trueparent = pdAnaUtils::GetTrueParticle(_spill->TrueParticles,truepart->ParentID);
-      if(trueparent)if(std::find(goodTrueParticles.begin(), goodTrueParticles.end(), trueparent) == goodTrueParticles.end())
-		   goodTrueParticles.insert(static_cast<AnaTrueParticleB*>(trueparent));
+      if(trueparent)
+        if(std::find(goodTrueParticles.begin(), goodTrueParticles.end(), trueparent) == goodTrueParticles.end())
+          goodTrueParticles.insert(static_cast<AnaTrueParticleB*>(trueparent));
     }
   }
-
+  
   // Transfer from the set to the vector
   _spill->TrueParticles.assign(goodTrueParticles.begin(), goodTrueParticles.end());
-  _savedTrueObjects += _spill->TrueParticles.size();
+  _savedTrueParticles += _spill->TrueParticles.size();
 }
--- src/secondaryKaonAnalysis/src/kaonCreateMiniTree.hxx
+++ src/secondaryKaonAnalysis/src/kaonCreateMiniTree.hxx
@@ -1,12 +1,12 @@
 #ifndef kaonCreateMiniTree_h
 #define kaonCreateMiniTree_h
 
-#include "SimpleLoopBase.hxx"
+#include "CreateMiniTree.hxx"
 #include "pdDataClasses.hxx"
 #include "InputManager.hxx"
 #include <set>
 
-class kaonCreateMiniTree: public SimpleLoopBase {
+class kaonCreateMiniTree: public CreateMiniTree {
  public:
 
   kaonCreateMiniTree(int argc, char *argv[]);
@@ -14,41 +14,12 @@ class kaonCreateMiniTree: public SimpleLoopBase {
 
 protected:
 
-  //---- These are mandatory functions
-  virtual bool Initialize     ();
-  virtual bool InitializeSpill(){return true;}
-
-  virtual void DefineOutputTree();
-
-  virtual void FinalizeSpill(){}
-  virtual void Finalize     ();
-
-  virtual bool Process();
-  //--------------------
-
-  virtual void FillMiniTree();
-
   bool RecoCandidateExists();
   bool TruthCandidateExists();
 
   void DeleteUninterestingParticles();
   void DeleteUninterestingTrueParticles();
 
-  enum miniTreeIndex{
-    minitree = OutputManager::enumSpecialTreesLast+1
-  };
-
-
-protected:
-
-  Int_t _totalRecoObjects;
-  Int_t _savedRecoObjects;
-  Int_t _totalTrueObjects;
-  Int_t _savedTrueObjects;
-
-  bool _saveCandidates;
-
-  AnaSpill* _spill;
 };
 
 #endif
