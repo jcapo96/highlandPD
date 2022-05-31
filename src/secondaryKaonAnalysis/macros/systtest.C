7d82a69b7648a5bc27c9e273b4f8ee1fbd2fa012
migarpez <migarpez@ific.uv.es>
2022-05-31 21:48:30 +0200

    script for propagating systematics on coherent fits

---
+++ src/secondaryKaonAnalysis/macros/systtest.C
@@ -0,0 +1,13 @@
+void systtest(){
+
+  gStyle->SetOptStat(0);
+  
+  CoherentFit* cf_mc = new CoherentFit("/home/miguel/Public/highland/files/mc/systematics/6GeV-7GeV_prod4a_filtered_syst_2022-05-30.root");
+  cf_mc->CreateCoherentSamples(84);
+  
+  cf_mc->GenerateTrueMCHistograms(1,31,2,84,2);
+  cf_mc->SetBackgroundModel(CoherentSample::BackgroundModelEnum::kQuadraticWidths);
+  cf_mc->SequentialCoherentFit();
+  cf_mc->PropagateSystematicErrors();
+}
+
