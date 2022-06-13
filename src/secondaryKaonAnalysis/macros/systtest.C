e359088df21493cdd5290d084daf8d404a9b00f1
migarpez <migarpez@ific.uv.es>
2022-06-13 18:04:15 +0200

    adf

--- src/secondaryKaonAnalysis/macros/systtest.C
+++ src/secondaryKaonAnalysis/macros/systtest.C
@@ -5,9 +5,9 @@ void systtest(){
   CoherentFit* cf_mc = new CoherentFit("/home/miguel/Public/highland/files/mc/systematics/6GeV-7GeV_prod4a_filtered_syst_2022-05-30.root");
   cf_mc->CreateCoherentSamples(84);
   
-  cf_mc->GenerateTrueMCHistograms(1,31,2,84,2);
+  cf_mc->GenerateTrueMCHistograms(1,5,2,84,2);
   cf_mc->SetBackgroundModel(CoherentSample::BackgroundModelEnum::kQuadraticWidths);
   cf_mc->SequentialCoherentFit();
-  cf_mc->PropagateSystematicErrors();
+  cf_mc->PropagateSystematicErrors(true,true);
 }
 
