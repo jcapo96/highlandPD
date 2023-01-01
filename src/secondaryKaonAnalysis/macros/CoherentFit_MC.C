8c7702c366e46c171ab0bd8c23ad4584e92f3659
Miguel Garcia Peris <miagarc@dunegpvm12.fnal.gov>
2023-01-01 10:03:47 -0600

    macro for phd writing

--- src/secondaryKaonAnalysis/macros/CoherentFit_MC.C
+++ src/secondaryKaonAnalysis/macros/CoherentFit_MC.C
@@ -3,13 +3,12 @@
 
 void CoherentFit_MC(){
 
-  gStyle->SetOptStat(0);
-  
-  CoherentFit* cf_mc = new CoherentFit("../../../../test.root");
+  CoherentFit* cf_mc = new CoherentFit("/dune/data2/users/miagarc/microtrees/dunesw_v09_56_00d00/6-7GeV_prod4a_microtree_2022-12-30.root");
   cf_mc->CreateCoherentSamples(84);
   
   cf_mc->GenerateTrueMCHistograms(1,31,2,84,2);
   cf_mc->SetBackgroundModel(CoherentSample::BackgroundModelEnum::kQuadraticWidths);
   cf_mc->SequentialCoherentFit();
 
+  cf_mc->WriteToRootFile("CoherentFit_MC_2022-12-30"); 
 }
