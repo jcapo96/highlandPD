70ed6feab2d3b0ecbbc7544951002d512415451b
migarpez <migarpez@ific.uv.es>
2023-08-07 15:07:30 +0100

    macro change

--- src/secondaryKaonAnalysis/macros/selfsysttest.C
+++ src/secondaryKaonAnalysis/macros/selfsysttest.C
@@ -1,14 +1,18 @@
 void selfsysttest(){
 
   gStyle->SetOptStat(0);
+
+  CoherentFit* cf_mc = new CoherentFit("/home/migue/Documents/PhD/tesis/files/microtrees/systematics/6-7GeV_prod4a_microtree_syst_beamMom_4-00_1toy_2023-04-06.root",true);
+  cf_mc->CreateCoherentSamples(999);
+  cf_mc->GenerateTrueMCHistograms(1,61,2,999,1,20);
+  CoherentSample* sb_sample = cf_mc->GetSignalPlusBackgroundSample();
+  CoherentSample* ts_sample = cf_mc->GetTrueSignalSample();
+  CoherentSample* tb_sample = cf_mc->GetTrueBackgroundSample();
+
+  ts_sample->SequentialCoherentFit();
+  tb_sample->SequentialCoherentFit();
+  sb_sample->GetInitialParValuesForCoherentFit();
   
-  CoherentFit* cf_mc = new CoherentFit("../../../../test.root");
-  cf_mc->CreateCoherentSamples(84);
-  
-  cf_mc->GenerateTrueMCHistograms(1,31,2,84,2);
-  cf_mc->SetBackgroundModel(CoherentSample::BackgroundModelEnum::kQuadraticWidths);
-  cf_mc->SequentialCoherentFit();
-  cf_mc->InitializeHistogramsForSystematicErrors();
-  cf_mc->ComputeSelfSystematicError(false,false,false,false,false,false,false,true);
+  cf_mc->ComputeSelfSystematicError();
 }
 
