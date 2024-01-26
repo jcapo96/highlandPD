6d6aa3ca7324ba485a7d1eb1cb2ea49f6ffce2c9
Miguel Garcia Peris <miagarc@dunegpvm12.fnal.gov>
2024-01-26 07:28:01 -0600

    flat dedx systematic

---
+++ src/pdSystematics/src/dEdxVariation.cxx
@@ -0,0 +1,91 @@
+#include "dEdxVariation.hxx"
+#include "pdAnalysisUtils.hxx"
+
+//********************************************************************
+dEdxVariation::dEdxVariation():EventVariationBase(),BinnedParams(std::string(getenv("PDSYSTEMATICSROOT"))+"/data","dEdx", BinnedParams::k1D_SYMMETRIC_NOMEAN){
+//********************************************************************
+
+  // Read the systematic source parameters from the data files
+  SetNParameters(GetNBins());
+
+  // Initialize Calorimetry object
+  _cal = new Calorimetry();
+  _cal->Initialize();
+ 
+}
+
+//********************************************************************
+void dEdxVariation::Apply(const ToyExperiment& toy, AnaEventC& event){
+//********************************************************************
+
+  // Get the SystBox for this event. The SystBox contains vector of objects that are relevant for this systematic.
+  // In this way the loop over objects is restricted to the ones that really matter
+  SystBoxB* box = GetSystBox(event);
+
+  // to get the uncertantainty
+  BinnedParamsParams params;
+  int index;
+  
+  // Loop over all relevant tracks for this variation
+  for(Int_t ipart = 0; ipart < box->nRelevantRecObjects; ipart++){
+
+    AnaParticlePD* part = static_cast<AnaParticlePD*>(box->RelevantRecObjects[ipart]);
+    if(part->Hits[2].empty())continue;
+
+    // The un-varied particle
+    const AnaParticlePD* original = static_cast<const AnaParticlePD*>(part->Original);
+    if(!original)continue;
+    //loop over hits
+    for(int ihit = 0; ihit < (int)part->Hits[2].size(); ihit++){
+      if(!GetBinValues(0.5, params, index))return;
+      double width = params.sigma;
+      //get the calibration
+      part->Hits[2][ihit].dEdx = part->Hits[2][ihit].dEdx * (1 + width*toy.GetToyVariations(_index)->Variations[index]);
+      //recompute dEdx
+    }
+    
+    //recompute derived quantities
+    std::pair<double, int> chi2pidprot = pdAnaUtils::Chi2PID(*part, 2212);
+    part->Chi2Proton  = chi2pidprot.first;
+    std::pair<double, int> chi2pidmuon = pdAnaUtils::Chi2PID(*part, 13);
+    part->Chi2Muon    = chi2pidmuon.first;
+    part->Chi2ndf     = chi2pidmuon.second;
+  }
+}
+
+//********************************************************************
+bool dEdxVariation::UndoSystematic(AnaEventC& event){
+//********************************************************************
+
+  // Get the SystBox for this event
+  SystBoxB* box = GetSystBox(event);
+
+  for(Int_t ipart = 0; ipart < box->nRelevantRecObjects; ipart++){
+    AnaParticlePD* part = static_cast<AnaParticlePD*>(box->RelevantRecObjects[ipart]);
+    const AnaParticlePD* original = static_cast<const AnaParticlePD*>(part->Original);
+    if(!original)continue;
+
+    //loop over hits
+    for(int ihit = 0; ihit < (int)part->Hits[2].size(); ihit++)
+      part->Hits[2][ihit].dEdx = original->Hits[2][ihit].dEdx;
+    
+
+    // reset the top level quantities
+    part->Chi2Proton = original->Chi2Proton;
+    part->Chi2ndf    = original->Chi2ndf;
+    part->Chi2Muon   = original->Chi2Muon;
+  }
+  
+  // Don't reset the spill to corrected
+  return false;
+}
+
+//**************************************************
+bool dEdxVariation::IsRelevantRecObject(const AnaEventC& event, const AnaRecObjectC& part) const{
+//**************************************************
+
+  (void)event;
+  
+  //the systematic box only includes candidates and daughters, and all of them are relevant
+  return true;
+}
